f2ee1cf5ef26f3e6f1582a76c1ac3de05cd22ad5
amritrajputt <vishal179singh@gmail.com>
2025-12-06 22:15:26 +0530

    week 9-2

---
+++ week-9/week-9.2/week 9.2/src/App.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+    useEffect(() => {
+     const id =  setInterval(() => {
+        setCount(count => count+1)
+      },1000)
+      
+    }, [])
+
+  function IncreaseCount(){
+    setCount(count+1)
+  }
+
+   return (
+    <>
+      <div>counter app that changes count every 1 second using useEffect</div>
+      <h1>{count}</h1>
+      <button onClick={IncreaseCount}>Increase Count</button>
+    </>
+  )
+}
+
+export default App
