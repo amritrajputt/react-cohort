ddd87b22e176ff06b06438be94860bf7dc37d0ed
amritrajputt <vishal179singh@gmail.com>
2025-12-22 23:14:25 +0530

    push

---
+++ react/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  return (
+    <div>
+      foodVilla
+      <Header/>
+    </div>
+  )
+}
+
+function Header(){
+  return (
+  <div style={{display:'flex',float:'right', justifyContent:'space-between', margin:'10px', border: "1px solid black"}}>
+<App/>
+     <div >
+          
+      <ul style={{listStyle:'none', display:'flex'}}>
+        <li style={{padding:'10px'}}>Home</li>
+        <li style={{padding:'10px'}}>About</li>
+        <li style={{padding:'10px'}}>Contact</li>
+        <li style={{padding:'10px'}}>Cart</li>
+      </ul>
+     </div>
+    </div>
+  )
+}
+
+export default App
